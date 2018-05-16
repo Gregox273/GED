be0af904c468564475db2e20e00688374c27e419
dr446 <dr446@cam.ac.uk>
2018-05-16 23:12:42 +0100

    Add files via upload
    I think this works now

---
+++ up-to-date-reconstruction-code/ram_lak.m
@@ -0,0 +1,25 @@
+function [Y]=ram_lak(m)
+
+filter = -(m-1)/2:(m-1)/2;
+w_max = pi;
+
+delta_w = 2*w_max/m;
+
+filter = abs(filter*delta_w);
+
+for i=1:m-1
+    filter(i) = mean([filter(i), filter(i+1)]);
+end
+
+Y = filter
+
+stairs(-(m-1)/2:(m-1)/2,filter)
+
+%for 
+
+%ram_lak_filter = abs(filter)/(2*pi).*cos(filter./w_max*pi/2).^0.001;
+
+
+
+%size(ram_lak_filter)
+%stairs([-(m-1)/2:(m-1)/2], ram_lak_filter);
