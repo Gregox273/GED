3a0e3ed17f0ae7fcfddf073782f0dfdcdee7229d
Gregory Brooks <gregox273@gmail.com>
2018-05-17 13:12:36 +0100

    Correct filter alignment and tidy ram lak function

--- ct_matlab/ram_lak.m
+++ ct_matlab/ram_lak.m
@@ -1,22 +1,22 @@
 function [Y]=ram_lak(m, scale)
 
-filter = -(m-1)/2:(m-1)/2;
+filter = -(m)/2:(m-2)/2;
 w_max = pi/scale;
 
 delta_w = 2*w_max/m;
 
 filter = abs(filter*delta_w);
 
-for i=1:m-1
-    filter(i) = mean([filter(i), filter(i+1)]);
-end
+% for i=1:m-1
+%     filter(i) = mean([filter(i), filter(i+1)]);
+% end
 
-filter(m/2) = delta_w/4;
+filter(m/2+1) = delta_w/4;
 
-stairs(-(m-1)/2:(m-1)/2,filter);
+%stairs(-(m-1)/2:(m-1)/2,filter);
 
 
 Y = abs(filter)/(2*pi).*cos(filter./w_max*pi/2).^0.001;
 
 
-stairs(-(m-1)/2:(m-1)/2, Y);
+%stairs(-(m-1)/2:(m-1)/2, Y);
