dd461c3731a32c794f4e637ea553486bc4acef43
Gregory Brooks <gregox273@gmail.com>
2018-05-28 22:25:56 +0100

    Add hounsfield units approximation to xtreme_reconstruct_all

--- ct_matlab/xtreme_run.m
+++ ct_matlab/xtreme_run.m
@@ -1,2 +1,2 @@
-h = xtreme_get_rsq_header('../raw_ct_data/test_object_data.rsq');
-xtreme_reconstruct_all(h,'../dicom/test_object_data', 0.001, material);
+h = xtreme_get_rsq_header('../raw_ct_data/raw_data_a.rsq');
+xtreme_reconstruct_all(h,'../dicom/raw_data_a', 0.001, material);
