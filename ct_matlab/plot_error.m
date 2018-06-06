eda3e18a94b742f0c5505c18c65ccf0e2d362755
Greg Brooks <gregox273@gmail.com>
2018-06-06 15:06:21 +0100

    Update plot_error to exclude skip_scans and use % error = (hypotenuse - radius)/(hypotenuse)

--- ct_matlab/plot_error.m
+++ ct_matlab/plot_error.m
@@ -1,6 +1,7 @@
 h = xtreme_get_rsq_header('../raw_ct_data/raw_data_b.rsq');
-mid = (h.fan_scans+1)/2;
-x = [1:h.fan_scans];
+mid = (h.fan_scans-2*h.skip_scans+1)/2;
+x = [1:h.fan_scans-2*h.skip_scans];
 y = abs(mid - x);
-y = sqrt((y.^2 * h.scale^2)/(h.radius^2) + 1);
-plot(100*([y,y,y]-1));
+%y = sqrt((y.^2 * h.scale^2)/(h.radius^2) + 1);
+y = sqrt((h.radius^2)./((y.^2 * h.scale^2) + h.radius^2));
+plot(100*(1-[y,y,y]));
